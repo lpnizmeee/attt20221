151915d492300b4f2ee76cd8c12b5061b3ac9727
lephuongnam <pnam020103@gmail.com>
2022-12-09 12:39:27 +0700

    first commit

---
+++ ecc.py
@@ -0,0 +1,70 @@
+from tinyec import registry
+from Crypto.Cipher import AES
+import hashlib, secrets, binascii
+
+def encrypt_AES_GCM(msg, secretKey):
+    aesCipher = AES.new(secretKey, AES.MODE_GCM)
+    ciphertext, authTag = aesCipher.encrypt_and_digest(msg)
+    return (ciphertext, aesCipher.nonce, authTag)
+
+def decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey):
+    aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
+    plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
+    return plaintext
+
+def ecc_point_to_256_bit_key(point):
+    sha = hashlib.sha256(int.to_bytes(point.x, 32, 'big'))
+    sha.update(int.to_bytes(point.y, 32, 'big'))
+    return sha.digest()
+
+curve = registry.get_curve('brainpoolP256r1')
+
+def encrypt_ECC(msg, pubKey):
+    ciphertextPrivKey = secrets.randbelow(curve.field.n)
+    sharedECCKey = ciphertextPrivKey * pubKey
+    secretKey = ecc_point_to_256_bit_key(sharedECCKey)
+    ciphertext, nonce, authTag = encrypt_AES_GCM(msg, secretKey)
+    ciphertextPubKey = ciphertextPrivKey * curve.g
+    return (ciphertext, nonce, authTag, ciphertextPubKey, ciphertextPrivKey)
+
+def decrypt_ECC_admin(encryptedMsg, privKey):
+    (ciphertext, nonce, authTag, ciphertextPubKey, ciphertextPrivKey) = encryptedMsg
+    sharedECCKey = privKey * ciphertextPubKey
+    secretKey = ecc_point_to_256_bit_key(sharedECCKey)
+    plaintext = decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey)
+    return plaintext
+
+def decrypt__ECC_user(encryptedMsg, pubKey):
+    (ciphertext, nonce, authTag, ciphertextPubKey, ciphertextPrivKey) = encryptedMsg
+    sharedECCKey = pubKey * ciphertextPrivKey
+    secretKey = ecc_point_to_256_bit_key(sharedECCKey)
+    plaintext = decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey)
+    return plaintext
+
+msg = b'Text to be encrypted by ECC public key and ' \
+      b'decrypted by its corresponding ECC private key'
+print("*******************")
+print("original msg:", msg)
+print("*******************")
+privKey = secrets.randbelow(curve.field.n)
+pubKey = privKey * curve.g
+
+encryptedMsg = encrypt_ECC(msg, pubKey)
+
+encryptedMsgObj = {
+    'ciphertext': binascii.hexlify(encryptedMsg[0]),
+    'nonce': binascii.hexlify(encryptedMsg[1]),
+    'authTag': binascii.hexlify(encryptedMsg[2]),
+    'ciphertextPubKey': hex(encryptedMsg[3].x) + hex(encryptedMsg[3].y % 2)[2:],
+    'ciphertextPrivKey': hex(encryptedMsg[4])
+}
+print("encrypted msg:")
+for keys,values in encryptedMsgObj.items():
+    print(keys, ": ",values)
+print("*******************")
+decryptedMsg__admin = decrypt_ECC_admin(encryptedMsg, privKey)
+decryptedMsg__user = decrypt__ECC_user(encryptedMsg, pubKey)
+print("decrypted msg__admin:", decryptedMsg__admin)
+print("*******************")
+print("decrypted msg__user:", decryptedMsg__user)
+print("*******************")
